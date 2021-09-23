aca11a036552e8ead4e9f05ef3761368c6fb4da5
nathakrit061103jnt <std60010912540@acc.msu.ac.th>
2021-09-23 12:53:12 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Container } from "@material-ui/core";
+import Header from "./components/Header";
+import Menu from "./components/Menu";
 
-function App() {
+import { makeStyles } from "@material-ui/core/styles";
+
+import Box1 from "./components/Box1";
+
+import Data from "./components/Data";
+
+import Form from "./components/Form";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    flexGrow: 1,
+    display: "flex",
+    justifyContent: "center",
+    padding: theme.spacing(20),
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+  const [openDrawer, setOpenDrawer] = React.useState(true);
+
+  const handleDrawerClose = () => {
+    setOpenDrawer(false);
+  };
+
+  const handleDrawerChange = () => {
+    !openDrawer ? setOpenDrawer(true) : setOpenDrawer(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header handleDrawerChange={handleDrawerChange} open={openDrawer} />
+      <Menu open={openDrawer} handleDrawerClose={handleDrawerClose} />
+      <Container className={classes.content}>
+        <Switch>
+          <Route path="/box1" component={Box1} />
+          <Route path="/data" component={Data} />
+          <Route path="/form" component={Form} />
+          <Route path="/signIn" component={SignIn} />
+          <Route path="/signUp" component={SignUp} />
+          <Route path="/form" component={Form} />
+          <Route
+            exact={true}
+            path="/"
+            component={() => <Redirect to="/login" />}
+          />{" "}
+        </Switch>
+      </Container>
+    </Router>
   );
 }
-
-export default App;
